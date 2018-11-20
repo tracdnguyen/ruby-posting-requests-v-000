899455f1b22b2529faa0c8a4cfef3c62fca90646
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 05:05:45 +0000

    automatically backed up by learn

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -6,7 +6,7 @@ class TipsController < ApplicationController
     end
     @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
   end
-  
+
   def create
     resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
       req.params['oauth_token'] = session[:token]
